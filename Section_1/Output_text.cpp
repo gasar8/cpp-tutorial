02ca3b825e3d01151691c4467402c45efba6bdae
Blaz Leban <blaz.leban@cern.ch>
2020-07-16 10:54:45 +0200

    Course 5

---
+++ Section_1/Output_text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Starting program ..." << endl;
+    cout << "This is some text." << endl;
+    cout << "Banana." << " Apple." << " Orange. " << flush;
+    cout << "This is some other text." << endl;
+    return 0;
+}
