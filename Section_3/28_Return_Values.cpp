a267c74ddd3991be972ec39042a3f02d30a509f4
Blaz Leban <blaz.leban@cern.ch>
2020-07-20 10:31:26 +0200

    Course 28

---
+++ Section_3/28_Return_Values.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void showMenu()
+{
+    cout << "1. Search" << endl;
+    cout << "2. View Record" << endl;
+    cout << "3. Quit" << endl;
+    cout << endl;
+}
+
+int getInput()
+{
+    int input;
+    cout << "Enter selection > " << flush;
+    cin >> input;
+    
+    return input;
+}
+
+int main()
+{
+    showMenu();
+    int selection = getInput();
+
+    switch (selection)
+    {
+    case 1:
+        cout << "Searching ..." << endl;
+        break;
+    case 2:
+        cout << "Viewing ..." << endl;
+        break;
+    case 3:
+        cout << "Quitting ..." << endl;
+        break;
+    default:
+        cout << "Please select an item from the menu." << endl;
+    }
+
+    return 0;
+}
