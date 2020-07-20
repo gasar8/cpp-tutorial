102770c42ebaa8ee3c52c81edb13d967747d47c1
Blaz Leban <blaz.leban@cern.ch>
2020-07-20 12:54:50 +0200

    Course 32

---
+++ Section_4/32_Data_Members/32_Data_Members.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+int main()
+{
+    Cat jim;
+    jim.makeHappy();
+    jim.speak();
+
+    Cat bob;
+    bob.makeSad();
+    bob.speak();
+
+    return 0;
+}
---
+++ Section_4/32_Data_Members/Cat.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak()
+{
+    if (happy)
+    {
+        cout << "Meouw!!! " << endl;
+    }
+    else
+    {
+        cout << "Sssss!!! " << endl;
+    }
+}
+
+void Cat::makeHappy()
+{
+    happy = true;
+}
+
+void Cat::makeSad()
+{
+    happy = false;
+}
